df2ecc74fcab1af58dcd9fa407e46156659a4b28
Michael Slevin <michaelslevin@Michaels-MacBook-Pro.local>
2019-01-21 11:43:34 +0000

    first commit

---
+++ Library/Tests/TestingAdditionSpec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Library.Models;
+
+namespace Library.Tests
+{
+    [TestFixture]
+    public class Testing_addition
+    {
+        //public EmptyClass()
+        //{
+        //}
+        [Test(Description = "1=1")]
+        public void Testing_addition1()
+        {
+        //arrange
+
+
+        //act
+
+        //assert
+        Assert.AreEqual(2, 1+1);
+        }
+    }
+}
