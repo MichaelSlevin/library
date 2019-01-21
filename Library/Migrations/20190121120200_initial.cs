0d792074c05eb92ae60778e9c8d5c0762063b63a
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2019-01-21 12:46:13 +0000

    first two migrations files added, still need to complete ContextModelSnapshot and a LibraryContext model

---
+++ Library/Migrations/20190121120200_initial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Library.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Author = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    ISBN = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<long>(nullable: false),
+                    Available = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    FullName = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Id", x => x.Id);
+                });
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+            protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Library/Migrations/LibraryContextModelSnapshot.cs
@@ -0,0 +1,10 @@
+﻿//using System;
+//namespace Library.Migrations
+//{
+//    public class LibraryContextModelSnapshot
+//    {
+//        public LibraryContextModelSnapshot()
+//        {
+//        }
+//    }
+//}
