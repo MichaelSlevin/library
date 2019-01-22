c355a15382eb43efd97bd94b4bcaa4073a280117
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2019-01-22 15:02:47 +0000

    Signup implemented

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Library.Models;
 
 namespace Library.Controllers
@@ -19,19 +20,39 @@ namespace Library.Controllers
         }
 
         public IActionResult Index()
+        { 
+            return View();
+        }
+
+        public IActionResult Register()
         {
+            return View();
+        }
+
+        public IActionResult AddUser(string fullName, string username, string email, string phoneNumber, string password)
+        {
+            Console.WriteLine($"Username is {username}");
+            Console.WriteLine($"Email is {email}");
+            Console.WriteLine("The form is triggering the post");
             User newUser = new User
             {
-                FullName = "test user",
-                Username = "testUser1",
-                Email = "test@test.com",
-                PhoneNumber = "12345",
-                Password = "abcde"
+                FullName = fullName,
+                Username = username,
+                Email = email,
+                PhoneNumber = phoneNumber,
+                Password = password
             };
             _context.Users.Add(newUser);
             _context.SaveChanges();
-
             HttpContext.Session.SetObjectAsJson("newUser", newUser);
+
+            return Redirect($"Profile/{newUser.Username}");
+        }
+
+        public IActionResult Profile(string username)
+        {
+            User newUser = HttpContext.Session.GetObjectFromJson<User>("newUser");
+            @ViewData["newUser"] = newUser;
             return View(newUser);
         }
 
@@ -61,14 +82,6 @@ namespace Library.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddABook(string author, string title, string isbn, long ownerid)
-        {
-            _context.Books.Add(new Book { Author = author, Title = title, ISBN = isbn, OwnerId = ownerid  });
-            _context.SaveChanges();
-            return Redirect("/");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
