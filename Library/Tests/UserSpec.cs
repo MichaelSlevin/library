04f03195758420e53eea86c96d7ad99f01f73c03
Michael Slevin <michaelslevin@Michaels-MacBook-Pro.local>
2019-01-23 14:24:53 +0000

    user unit test

---
+++ Library/Tests/UserSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Library.Models;
+
+namespace Library.Tests
+{
+    public class UserSpec
+    {
+        [Test(Description = "User has a Username")]
+
+        public void HasUsername()
+        {
+            // arrange
+            User user = new User
+            {
+                FullName = "Test User",
+                Username = "TestUser",
+                Email = "Email@Email.com",
+                PhoneNumber = "0788161523715",
+                Password = "password"
+            };
+            // act
+
+            // assert
+            Assert.AreEqual(user.Username, "TestUser");
+        }
+    }
+}
