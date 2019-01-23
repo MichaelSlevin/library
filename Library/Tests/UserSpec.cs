5be65ff08039845e7f3f5e7899c8c84404898488
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2019-01-23 14:39:57 +0000

    book unit tests

--- Library/Tests/UserSpec.cs
+++ Library/Tests/UserSpec.cs
@@ -6,9 +6,9 @@ namespace Library.Tests
 {
     public class UserSpec
     {
-        [Test(Description = "User has a Username")]
+        [Test(Description = "User has details")]
 
-        public void HasUsername()
+        public void HasDetails()
         {
             // arrange
             User user = new User
@@ -22,7 +22,11 @@ namespace Library.Tests
             // act
 
             // assert
+            Assert.AreEqual(user.FullName, "Test User");
             Assert.AreEqual(user.Username, "TestUser");
+            Assert.AreEqual(user.Email, "Email@Email.com");
+            Assert.AreEqual(user.PhoneNumber, "0788161523715");
+            Assert.AreEqual(user.Password, "password");
         }
     }
 }
