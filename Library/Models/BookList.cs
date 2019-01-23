b13b132f02fd355ceb2006dd6bb7af230a5d41a0
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2019-01-23 13:19:45 +0000

    BookList model added

---
+++ Library/Models/BookList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+    public class BookList
+    {
+        readonly LibraryContext _context;
+        public IEnumerable<Book> AllBooks;
+
+        public BookList(LibraryContext context)
+        {
+            this._context = context;
+            this.AllBooks = this._context.Books.ToList();
+        }
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return this.AllBooks;
+        }
+
+    }
+}
