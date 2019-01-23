7922e7f74afa16172ca44342ce36c871a881ea21
Michael Slevin <michaelslevin@Michaels-MacBook-Pro.local>
2019-01-23 11:14:05 +0000

    updated routing

--- Library/Tests/ProfileSpec.cs
+++ Library/Tests/ProfileSpec.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using NUnit.Framework;
-using Library.Models;
-
-namespace Library.Tests
-{
-    public class ProfileSpec
-    {
-        [Test(Description = "Profile has a Username")]
-
-        public void HasUsername()
-        {
-            // arrange
-            Profile profile = new Profile("User 1");
-            // act
-
-            // assert
-            Assert.AreEqual(profile.Username, "User 1");
-        }
-    }
-}
+// ﻿using System;
+// using NUnit.Framework;
+// using Library.Models;
+//
+// namespace Library.Tests
+// {
+//     public class ProfileSpec
+//     {
+//         [Test(Description = "Profile has a Username")]
+//
+//         public void HasUsername()
+//         {
+//             // arrange
+//             Profile profile = new Profile("User 1");
+//             // act
+//
+//             // assert
+//             Assert.AreEqual(profile.Username, "User 1");
+//         }
+//     }
+// }
