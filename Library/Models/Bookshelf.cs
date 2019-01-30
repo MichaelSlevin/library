f7d4f45f1d3564aac866ce84a5fc97cd1c66c6f9
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2019-01-30 17:52:59 +0000

    database join not working yet

--- Library/Models/Bookshelf.cs
+++ Library/Models/Bookshelf.cs
@@ -1,5 +1,5 @@
-
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,8 +16,11 @@ namespace Library.Models
 
         public User Owner;
         public long UserId;
+        public List<Book_info> bookInfos;
+        public IEnumerable<Book> myBooks;
         public bool NoAvailableBooks = true;
         public bool NoUnavailableBooks = true;
+        ArrayList myISBNArray = new ArrayList();
 
         public Bookshelf(long userid, LibraryContext context)
         {
@@ -25,6 +28,9 @@ namespace Library.Models
             this.UserId = userid;
             this._context = context;
             var books =  this._context.Books.ToList();
+            this.bookInfos = this._context.Book_info.ToList();
+            this.myBooks = books.Where(book => book.OwnerId == userid);
+
             this.AvailableBooks = books.Where(book => book.OwnerId == userid && book.Available == true);
             this.UnavailableBooks = books.Where(book => book.OwnerId == userid && book.Available == false);
 
@@ -37,17 +43,44 @@ namespace Library.Models
               this.NoUnavailableBooks = false;
             }
 
+            var books1 = _context.Set<Book>().AsQueryable();
+            var book_info1 = _context.Set<Book_info>().AsQueryable();
+            var resList = books1.GroupJoin(book_info1, book => book.ISBN, book_info => book_info.ISBN, (a, b) => new { Book = a, Book_info = b.DefaultIfEmpty() }).SelectMany(obj => obj.Book_info.Select(b => new { Book = obj.Book.Title, Book_info = b.ImageUrl })).ToList();
+            foreach(var i in resList)
+            {
+                Console.WriteLine(i.Book);
+                Console.WriteLine(i.Book_info);
+
+            }
+            //var resList1 = _context.Set<Book>().AsQueryable().Include(a => a.ISBN.Select(b => b.ISBN)).ToList();
+
         }
+
         public IEnumerable<Book> getAvailableBooks()
         {
           return this.AvailableBooks;
         }
+
         public IEnumerable<Book> getUnavailableBooks()
         {
           return this.UnavailableBooks;
         }
 
+        public ArrayList getMyISBNs()
+        {
+            foreach (var book in myBooks)
+            {
+                myISBNArray.Add(book.ISBN);
+            }
+            return myISBNArray; 
+        }
 
+        //public IEnumerable<Book_info> getUserBookInfo
+        //{ 
+       
+
+        ////var resList = authurs.GroupJoin(books, authur => authur.Id, book => book.AuthorId, (a, b) => new { Author = a, Book = b.DefaultIfEmpty() }).SelectMany(obj => obj.Book.Select(b => new { Author = obj.Author.Name, Book = b.Name })).ToList();
+        //}
 
     }
 }
